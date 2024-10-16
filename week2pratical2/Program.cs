2617b97ffceae4edb1bc404eea3abaed58a956ed
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-16 19:07:43 +0100

    Add project files.

---
+++ week2pratical2/Program.cs
@@ -0,0 +1,20 @@
+﻿Main();
+static void Main()
+{
+    Console.WriteLine("Enter a String:");
+    string str = Console.ReadLine();
+
+    int wordCount = 0;
+
+    for (int letter = 0; letter < str.Length; letter++)
+    {
+        if (char.IsUpper(str[letter]))
+        {
+            wordCount++;
+
+        }
+    }
+    Console.WriteLine($"This the string , {str}");
+    Console.WriteLine("Number of words: " + wordCount);
+
+}
